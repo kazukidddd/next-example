2aea726a7fbacc4b6107307f302b0a79f6adc892
kazuki_kimura <kazuki.kimura.dev@gmail.com>
2024-08-25 18:50:34 +0900

    refactor: Update Hero component to use simplified CSS classes

--- blog/src/components/hero.tsx
+++ blog/src/components/hero.tsx
@@ -3,7 +3,7 @@ import styles from './hero.module.css';
 export default function Hero({title, subtitle, imageOn = false}: {title: string, subtitle: string, imageOn?: boolean}) {
 	return (
 		<div className={styles.text}>
-			<h1>{title}</h1>
+			<h1 className={styles.title}>{title}</h1>
 			<p>{subtitle}</p>
 			{imageOn && <figure>画像</figure>}
 		</div>
