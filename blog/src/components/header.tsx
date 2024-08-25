555de5ef1c685e78705eca68cf42e56fbb2bbf50
kazuki_kimura <kazuki.kimura.dev@gmail.com>
2024-08-25 17:34:28 +0900

    refactor: Add Logo and Nav components to Header and Footer components

--- blog/src/components/header.tsx
+++ blog/src/components/header.tsx
@@ -1,5 +1,11 @@
+import Logo from "./logo";
+import Nav from "./nav";
+
 export default function Header() {
     return (
-        <header>HEADER</header>
+        <header>
+            <Logo />
+            <Nav />
+        </header>
     )
 }
