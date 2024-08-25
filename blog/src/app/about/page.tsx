0b1c292c2bf7ff8a246afebce7dec308809d4671
kazuki_kimura <kazuki.kimura.dev@gmail.com>
2024-08-25 16:13:28 +0900

    chore: Refactor About page component structure and update imports

--- blog/src/app/about/page.tsx
+++ blog/src/app/about/page.tsx
@@ -1,33 +1,8 @@
-import { EachPost } from "@/components/each_post"
+import Hero from "@/components/hero"
 
 // ブラウザやファイルの外部からアクセス可能にする変数や関数にはexportを使う
-export default function About() {
-  const propsww = { title: "asdf", url: "よう" }
-  const component = (
-    <>
-    <header>HEADER</header>
-
-    <main>
-      <h1>CUBE</h1>
-      <p>アウトプットしていくサイト</p>
-      <EachPost title="asdf" url="よう" />
-      <EachPost {...propsww} />
-      <Decoration>
-        <h1>こんにちは</h1>
-        <p>こんにちは</p>
-      </Decoration>
-    </main>
-    <footer>FOOTER</footer>
-    </>
-  )
-  return component
-}
-
-
-function Decoration({children}: {children: React.ReactNode}) {
+export default function Page() {
   return (
-    <div style={{ color: 'red'}}>
-     {children}
-    </div>
+    <Hero />
   )
 }
