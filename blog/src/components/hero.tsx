43bbcb503ba37bcdc7b8fdbf7ad1cb7bf1e0a2cf
kazuki_kimura <kazuki.kimura.dev@gmail.com>
2024-08-25 19:01:22 +0900

    refactor: Update Hero component styles and structure

--- blog/src/components/hero.tsx
+++ blog/src/components/hero.tsx
@@ -2,7 +2,7 @@ import styles from './hero.module.css';
 
 export default function Hero({title, subtitle, imageOn = false}: {title: string, subtitle: string, imageOn?: boolean}) {
 	return (
-		<div className={styles.textPastel}>
+		<div className={styles.textPastelDotted}>
 			<h1 className={styles.title}>{title}</h1>
 			<p>{subtitle}</p>
 			{imageOn && <figure>画像</figure>}
